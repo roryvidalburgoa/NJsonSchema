237486e521146a0f2a6089279be9df44129f3d17
Rory Vidal <rory.vidal@brightsoft.io>
2022-06-21 22:07:28 -0400

    use only net6

---
+++ src/NJsonSchema.CodeGeneration.CStructs/CStructsGenerator.cs
@@ -0,0 +1,19 @@
+﻿namespace NJsonSchema.CodeGeneration.CStructs
+{
+    public class CStructsGenerator : GeneratorBase
+    {
+        public CStructsGenerator(object rootObject, TypeResolverBase typeResolver, CodeGeneratorSettingsBase settings) : base(rootObject, typeResolver, settings)
+        {
+        }
+
+        protected override string GenerateFile(IEnumerable<CodeArtifact> artifacts)
+        {
+            return string.Empty;
+        }
+
+        protected override CodeArtifact GenerateType(JsonSchema schema, string typeNameHint)
+        {
+            return new CodeArtifact("test", CodeArtifactType.Class, CodeArtifactLanguage.CSharp, CodeArtifactCategory.Contract, "struct");
+        }
+    }
+}
